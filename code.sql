fb6a190decc40ee389e3ebf48b8e3bee958a4609
EspenKle <espenkleivane@gmail.com>
2018-06-12 13:45:36 +0200

    Update code.sql

--- code.sql
+++ code.sql
@@ -10,6 +10,8 @@ sql_insert = "insert into holder (note, published) values (?, ?)"
 # statment select
 sql_select_all = "select * from holder"
 sql_select_max_id = "select max(id) from holder"
+# statment update
+sql_update_note = "update holder set note = ? where id = ?"
 
 print("\nSQLite3 project " + str(datetime.now()) + " data\n")
 
@@ -45,6 +47,23 @@ def insert(note):
             timeNow = datetime.now()
             global sql_insert
             cur.execute(sql_insert, (format(note), timeNow))
+            conn.commit()
+            row = cur.fetchall()
+            msg = row
+    except sqlite3.OperationalError as e:
+        msg = e
+    return msg
+
+def update(note_content, note_id):
+    msg = ""
+    try:
+        conn = get_conn()
+        conn = sqlite3.connect(get_db())
+        with conn:
+            cur = conn.cursor()
+            timeNow = datetime.now()
+            global sql_update_note
+            cur.execute(sql_update_note, (format(note_content), note_id))
             conn.commit()
             row = cur.fetchall()
             msg = row
@@ -93,3 +112,4 @@ rv = select_all()
 for r in rv:
     print(r) 
 print(select_id())
+print(update("new note", 2))
